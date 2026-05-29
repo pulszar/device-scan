b5cd828290c7afbc966a2750aa2caf1eca9662df
Luke Mattson <lukemattson03@gmail.com>
2026-05-28 21:01:36 -0500

    User input functionality

--- device_info.ps1
+++ device_info.ps1
@@ -1,9 +1,19 @@
-$output = [PSCustomObject]@{
-    Name = Get-ComputerInfo | Select-Object OsName
-    Uptime = Get-ComputerInfo | Select-Object OsUptime
-    InstallDate = Get-ComputerInfo | Select-Object OsInstallDate
-    LargestProcess = (Get-Process | Sort-Object WS -Descending | Select-Object -Index 1).ProcessName
+param (
+    [string]$ComputerType
+)
 
+function GetInfo {
+    $output = [PSCustomObject]@{
+        Name = Get-ComputerInfo | Select-Object OsName
+        Uptime = Get-ComputerInfo | Select-Object OsUptime
+        InstallDate = Get-ComputerInfo | Select-Object OsInstallDate
+        LargestProcess = (Get-Process | Sort-Object WS -Descending | Select-Object -Index 1).ProcessName
+    }
+    $output | Format-List
 }
 
-$output | Format-List
+switch ($ComputerType) {
+    "Local" { $output = GetInfo }
+    default   { Write-Host "Usage: .\device_info.ps1 -ComputerType Local" } # will build out support for azure VMs
+}
+
