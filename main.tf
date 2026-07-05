d1bca2d4c054e83bc93fc1027b8fb567302882f0
Luke Mattson <lukemattson03@gmail.com>
2026-07-05 17:29:04 -0500

    Initiate Terraform implementation

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "vm-inventory" {
+  name         = "pulzsar/vm-inventory"
+  keep_locally = false
+}
+
+resource "docker_container" "vm-inventory" {
+  image = docker_image.vm-inventory.image_id
+  name  = "vm-inventory"
+#   ports {
+#     internal = 80
+#     external = 8000
+#   }
+}
